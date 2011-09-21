f6dafc460dbdd0261ea768d5847f537d78da4864
steveoh <sgourley@utah.gov>
2011-09-20 19:03:31 -0600

    dojo require parser

---
+++ DojoRequireParser.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+
+namespace DojoRequireParser
+{
+    class DojoRequireParser
+    {
+        static void Main(string[] args)
+        {
+            Regex exp = new Regex(@"dojo\.require\([""'][\w\.]+['""]\);?", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            var dictionary = new Dictionary<string, string>();
+
+            Console.WriteLine("dojo.require parser for MVC 2.0");
+            Console.WriteLine("Created by Steve Gourley : AGRC" + Environment.NewLine + Environment.NewLine);
+
+#if !DEBUG
+            var path = GetProjectPath();
+            var savePath = GetSavePath();
+#endif
+
+            var path = @"C:\inetpub\wwwroot\UDAF_FRESH";
+            var savePath = @"C:\inetpub\wwwroot\UDAF_FRESH\UDAF\Content\_build";
+
+#if DEBUG
+            Stopwatch sw = Stopwatch.StartNew();
+#endif
+
+            while (!Directory.Exists(path))
+            {
+                Console.WriteLine(path + " does not exists.  Please Re-enter.");
+
+                path = GetProjectPath();
+            }
+
+            var dir = new DirectoryInfo(path);
+
+            BuildDictionaryOfRequires(exp, dictionary, dir);
+
+#if DEBUG
+            sw.Stop();
+            Console.WriteLine("Time used (float): {0} ms", sw.Elapsed.TotalMilliseconds);
+#endif
+
+            Console.WriteLine("Number of Requires: {0}", dictionary.Keys.Count);
+            SaveRequiresDoc(savePath, dictionary);
+
+            Console.WriteLine("Process Complete. Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void SaveRequiresDoc(string savePath, Dictionary<string, string> dictionary)
+        {
+            Directory.CreateDirectory(savePath);
+
+            using (var file = File.CreateText(Path.Combine(savePath, "requires.js")))
+            {
+                file.WriteLine("dojo.provide('core.requires');" + Environment.NewLine);
+
+                foreach (var item in dictionary.OrderBy(x => x.Key))
+                {
+                    file.WriteLine(item.Key);
+                }
+            }
+        }
+
+        private static string GetSavePath()
+        {
+            Console.WriteLine("Enter the location to save requires:");
+
+            return Console.ReadLine().TrimEnd('\\');
+        }
+
+        private static void BuildDictionaryOfRequires(Regex exp, Dictionary<string, string> dictionary, DirectoryInfo dir)
+        {
+            foreach (var item in dir.EnumerateFiles("*.*", SearchOption.AllDirectories).
+                                     Where(x => x.Extension == ".aspx" || x.Extension == ".ascx"))
+            {
+                var text = File.ReadAllText(item.FullName);
+
+                MatchCollection matches = exp.Matches(text);
+                foreach (Match match in matches)
+                {
+                    var matchValue = match.Groups[0].Value;
+
+                    if (dictionary.ContainsKey(matchValue))
+                    {
+                        dictionary[matchValue] = string.Format("{0},{1}", dictionary[matchValue], item.Name);
+                    }
+                    else
+                    {
+                        dictionary.Add(matchValue, item.Name);
+                    }
+                }
+            }
+        }
+
+        private static string GetProjectPath()
+        {
+            Console.WriteLine("Enter the parent folder path to the project to parse:");
+
+            return Console.ReadLine().TrimEnd('\\');
+        }
+    }
+}
